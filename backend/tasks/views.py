e15e31d973bc58359d2c88d65c3183e59a98425d
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-07-25 20:25:42 +0300

    changes in the tasks folder

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+from .models import Todo
 # Create your views here.
+
+class TaskView(viewsets.ModelViewSet):
+    serializer_class=TaskSerializer
+    queryset=Todo.objects.all()
