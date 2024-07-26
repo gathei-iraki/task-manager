7bcc5bcc1b2e1a488bd4241bd24201fd6fdd0b1d
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-07-26 10:01:18 +0300

    added changes to db

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
-
+from tasks import views 
 router = DefaultRouter()
 router.register(r'tasks', views.TaskView, basename='task')
 
--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     description = models.CharField(max_length=500)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # Set a default user ID
 
     def __str__(self):
         return self.title
